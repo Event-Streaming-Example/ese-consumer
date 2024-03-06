2c1552ac5c654488496305ae78b1ddaa257264cf
Saumya Bhatt <saumya.bhatt106@gmail.com>
2024-03-06 15:13:16 +0530

    Bug fix

--- components/usecase/DummyUsecaseExample/impl_usecase.py
+++ components/usecase/DummyUsecaseExample/impl_usecase.py
@@ -7,7 +7,9 @@ class DummyUsecaseExample(UsecaseListener):
         super().__init__()
 
     def update(self, data, view_ctx, stats_ctx, view_config: ViewConfig):
-        view_ctx.write("Usecase received an update after polling")
+        view_ctx.write("Usecase received an update. Rebuilding view context")
+        stats_ctx.write("Usecase received an update, Rebuilding stats context")
 
     def view(self, view_ctx, stats_ctx):
-        ctx.write("Final data after polling")
+        view_ctx.write("Final data after polling")
+        stats_ctx.write("Final consumer starts after polling")
