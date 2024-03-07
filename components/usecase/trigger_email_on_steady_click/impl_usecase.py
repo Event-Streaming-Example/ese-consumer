e244678f68e3215f412461ff1f1d0e56acaa015c
Saumya Bhatt <saumya.bhatt106@gmail.com>
2024-03-07 14:54:01 +0530

    Code review

--- components/usecase/trigger_email_on_steady_click/impl_usecase.py
+++ components/usecase/trigger_email_on_steady_click/impl_usecase.py
@@ -8,22 +8,21 @@ from components.datasource.backend.models.response import EventIPSnapshotData, E
 from components.datasource.backend.models.interractor import Snapshot, IpSnapshotLog
 from components.datasource.backend.view_consumer_metrics import get_max_lag, view_lag_metrics
 from components.usecase.trigger_email_on_steady_click.models import EventMailTracker
+from components.usecase.trigger_email_on_steady_click.constant import THRESHOLD_CONSECUTIVE_EVENTS, THRESHOLD_LIMIT_IN_EPOCH_MILLI
+
 
 
 SNAPSHOT_LOG: IpSnapshotLog
-
 MARKED_SNAPSHOT_LOG = IpSnapshotLog()
 EVENT_MAIL_TRACKER: List[EventMailTracker] = []
 
 
-THRESHOLD_LIMIT_IN_EPOCH_MILLI :int = 1000
-THRESHOLD_CONSECUTIVE_EVENTS   :int = 3
-
-
 class TriggerEmailOnSteadyClick(UsecaseListener): 
+
 
     def __init__(self): 
         super().__init__()
+
 
     def update(self, data: List[EventIPSnapshotData], view_ctx, stats_ctx, view_config: MailerViewConfig): 
         self._format_data(self, data)
@@ -34,16 +33,21 @@ class TriggerEmailOnSteadyClick(UsecaseListener):
         self._find_and_send_mail(self)
         self._view_sent_emails(self, view_ctx)
 
+
     def view(self, view_ctx, stats_ctx): 
         global SNAPSHOT_LOG
+
         if view_ctx.button("Clear output"): 
             SNAPSHOT_LOG.clear()
+
         self._view_sent_emails(self, view_ctx)
         view_lag_metrics(stats_ctx)
+
 
     def _format_data(self, data: List[EventIPSnapshotData]): 
         global SNAPSHOT_LOG
         updated_data = IpSnapshotLog()
+
         for item in data: 
             logs:List[Snapshot] = []
             for log in item.event_logs: 
@@ -54,21 +58,22 @@ class TriggerEmailOnSteadyClick(UsecaseListener):
                     event_subtype    = log.entity.data.event
                 ))
             updated_data.append(ip=item.ip, logs=logs)
+
         SNAPSHOT_LOG = updated_data
     
 
-
-    def _filter_snapshots(self, response: IpSnapshotLog, marked: IpSnapshotLog) -> IpSnapshotLog:
+    def _filter_snapshots(self, response: IpSnapshotLog, marked: IpSnapshotLog) -> IpSnapshotLog: 
         available = response - marked
         return available.filter_by_event_subtype(EventSubType.KEY_PRESS_EVENT)
 
 
-
     def _get_consecutive_click_snapshots(self, data:List[Snapshot]) -> List[Snapshot]: 
-        consecutive_snapshots: list[Snapshot] = []
-        if(len(data) < THRESHOLD_CONSECUTIVE_EVENTS ): return consecutive_snapshots
-        consecutive_snapshots.append(data[0])
+        consecutive_snapshots                                                        : list[Snapshot] = []
 
+        if (len(data) < THRESHOLD_CONSECUTIVE_EVENTS ):
+            return consecutive_snapshots
+        
+        consecutive_snapshots.append(data[0])
         for i in range(1, len(data))                                  : 
             if len(consecutive_snapshots) == THRESHOLD_CONSECUTIVE_EVENTS: 
                 return consecutive_snapshots
@@ -78,30 +83,31 @@ class TriggerEmailOnSteadyClick(UsecaseListener):
                 consecutive_snapshots.append(data[i])
             else: 
                 consecutive_snapshots.clear()
+
         return consecutive_snapshots
     
+
     def _find_and_send_mail(self): 
         global SNAPSHOT_LOG, MARKED_SNAPSHOT_LOG, EVENT_MAIL_TRACKER
         filtered_snapshot: IpSnapshotLog = self._filter_snapshots(self, SNAPSHOT_LOG, MARKED_SNAPSHOT_LOG)
-
 
         for snapshot in filtered_snapshot.get_iterable():
             consecutive_logs = self._get_consecutive_click_snapshots(self, snapshot.logs)
             if len(consecutive_logs) != 0:
                 EVENT_MAIL_TRACKER.append(EventMailTracker(
-                    ip=snapshot.ip,
-                    logs=consecutive_logs,
-                    sent_mail=True
+                    ip        = snapshot.ip,
+                    logs      = consecutive_logs,
+                    sent_mail = True
                 ))
                 MARKED_SNAPSHOT_LOG.append(snapshot.ip, consecutive_logs)
 
 
     def _view_sent_emails(self, view_ctx):
         result = []
-        for item in EVENT_MAIL_TRACKER:
+        for item in EVENT_MAIL_TRACKER: 
             result.append({
-                "ip" : item.ip,
-                "instances" : [ts.timestamp for ts in item.logs],
-                "mail sent" : item.sent_mail
+                "ip"       : item.ip,
+                "instances": [ts.timestamp for ts in item.logs],
+                "mail sent": item.sent_mail
             })     
         view_ctx.json(result)
